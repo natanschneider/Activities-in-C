9b5b24c872f58e679ac0905641af2953cf3ba76d
Natan Schneider <natanschneider@protonmail.com>
2021-11-05 20:22:14 -0300

    Change in "cubo"

--- cubo.cpp
+++ cubo.cpp
@@ -1,8 +1,20 @@
 #include <stdio.h>
+
+int cubo (int num)
+{
+    int res;
+    res = (num * num * num);
+
+return res;   
+}
+
+    int valor, res;
+    printf("Digite um numero para saber seu cubo: \n");
+    scanf("%d",valor);
+    res = cubo(valor);
+    printf("\nO cubo é: %d",res);
+
 int main()
 {
-
-
-
 return 0;    
 }
