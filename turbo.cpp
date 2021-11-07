1af2b5927cdcfb21b0fbad50ddce1ceb0ca7e001
Natan Schneider <natanschneider@protonmail.com>
2021-11-07 15:29:27 -0300

    new file

---
+++ turbo.cpp
@@ -0,0 +1,12 @@
+// Implemente uma calculadora em C que receba numeros continos seguidos de suas operações
+// onde que a cada operação ele vai tranformado em um resultado.
+// Exemplo: 2+3-8*2 = -2
+
+#include <stdio.h>
+
+int main()
+{
+
+
+return 0;
+}
