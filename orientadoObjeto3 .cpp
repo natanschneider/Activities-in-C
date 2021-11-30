2082465ce6dc38e91f9d1685230995d42d236555
Natãn Schneider <93141722+natanschneider@users.noreply.github.com>
2021-11-30 20:13:46 -0300

    'orientadoObjeto3' commit

--- orientadoObjeto3 .cpp
+++ orientadoObjeto3 .cpp
@@ -10,18 +10,17 @@ class vetores{
 
 int vetores::vet(int vet[5]){
     for (i=0;i<5;i++){
-        cout << "Digite o valor: "<< endl;
         cin >> vet[i];
 } 
 
 for (i=0;i<5;i++){
     for (j=i;j<5;j++){
-            if(vet[i] == vet[j]){
-                if(i != j){
-                    cout << "O numero repetido eh: " << vet[i] <<  " na posicao: " << i+1 << " e " << j+1 << endl;
-                    break;
-                }
+        if(vet[i] == vet[j]){
+            if(i != j){
+                cout << "O numero repetido eh: " << vet[i] <<  " na posicao: " << i+1 << " e " << j+1 << endl;
+                break;
             }
+        }
     }
 }
 }
@@ -30,6 +29,6 @@ int main(){
     int vet[5];
     vetores vetus;
     vetus.vet(vet);
-    return 0;
+return 0;
 }
 
