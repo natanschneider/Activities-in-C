0d98229ba33a0d5888077e6b9793fd715a7b1d49
Natãn Schneider <natanschneider@protonmail.com>
2021-11-30 23:27:24 -0300

    'merge' commit

---
+++ merge.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class merge{
+	public:
+	    int arr[5];
+	    int segArr[5];
+	    int terArr[11];
+	    int i, j;
+};
+
+int merge::vet(int arr[5]){
+	for(i=0; i<5; i++){
+		cin >> arr[i];
+	}
+	
+	for(j=0; j<5; j++){
+		cin >> segArr[j];
+	}
+	
+	//terArr = arr + segArr;
+	
+	
+	
+}	  
+
+int main(){
+	
+return 0;	
+}
+
